0192da377678fba149fb16b0549efb4e4af23691
Stefano Pisano <s.pisano91@gmail.com>
2017-12-12 23:15:15 +0100

    desire list

--- src/app/desire-element/desire-element.component.ts
+++ src/app/desire-element/desire-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Desire } from '../Model/Desire';
 
 @Component({
   selector: 'app-desire-element',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./desire-element.component.css']
 })
 export class DesireElementComponent implements OnInit {
+
+  @Input() desire : Desire;
 
   constructor() { }
 
--- src/app/desire/desire.component.ts
+++ src/app/desire/desire.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { DesireService } from './desire.service';
+import { Exception } from '../exception/Exception';
+import { Desire } from '../Model/Desire';
 
 @Component({
   selector: 'app-desire',
   templateUrl: './desire.component.html',
-  styleUrls: ['../home/home.component.css'],
-  
+  styleUrls: ['./desire.component.css', '../home/home.component.css'],
+  providers: [DesireService, Exception]
 })
 export class DesireComponent implements OnInit {
 
-  constructor() { }
+  listOfDesire : Desire[];
+
+  constructor(private desireService : DesireService, private exception : Exception) { 
+    this.getDesire();
+  }
 
   ngOnInit() {
   }
 
+  getDesire() {
+    this.desireService.getDesire()
+    .subscribe(
+      res => this.listOfDesire = JSON.parse(res._body),
+      err => console.log(err)
+    )
+  }
 }
